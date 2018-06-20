cf42f6d1cba807f6d6286d99e3519a1f0ce0c3e5
XanderVi <engineermexanic@gmail.com>
2018-06-20 20:16:04 +0300

    change the mission

--- verification/tests.py
+++ verification/tests.py
@@ -1,8 +1,8 @@
 init_code = """
-if not "AbstractArmy" in USER_GLOBAL:
-    raise NotImplementedError("Where is 'AbstractArmy'?")
+if not "Army" in USER_GLOBAL:
+    raise NotImplementedError("Where is 'Army'?")
 
-AbstractArmy = USER_GLOBAL['AbstractArmy']
+Army = USER_GLOBAL['Army']
 
 if not "AsianArmy" in USER_GLOBAL:
     raise NotImplementedError("Where is 'AsianArmy'?")
@@ -28,6 +28,12 @@ if not "Archer" in USER_GLOBAL:
     raise NotImplementedError("Where is 'Archer'?")
 
 Archer = USER_GLOBAL['Archer']
+
+if not issubclass(AsianArmy, Army):
+    raise Warning("AsianArmy should be the subclass of the Army")
+
+if not issubclass(EuropeanArmy, Army):
+    raise Warning("EuropeanArmy should be the subclass of the Army")
 """
 
 run_test = """
@@ -50,42 +56,74 @@ def prepare_test(test="", answer=None, middle_code="", show_code=None):
 TESTS = {
     "1. My army": [
         prepare_test(middle_code='''my_army = EuropeanArmy()
-my_army.add_swordsman("Jaks")
-my_army.add_swordsman("Brin")
-my_army.add_lancer("Harold")
-my_army.add_archer("Robin")
-my_army.add_archer("May")''',
-                     test="my_army.show_army()",
-                     answer={"Knights": 2, "Raubritters": 1, "Rangers": 2, "Total": 5})
+soldier_1 = my_army.train_swordsman("David")''',
+                     test="soldier_1.introduce()",
+                     answer="Knight David, Europen swordsman")
         ],
     "2. My army": [
         prepare_test(middle_code='''my_army = EuropeanArmy()
-my_army.add_swordsman("Jaks")
-my_army.add_swordsman("Brin")
-my_army.add_lancer("Harold")
-my_army.add_archer("Robin")
-my_army.add_archer("May")''',
-                     test="my_army.show_swordsmans()",
-                     answer=["Knight Jaks", "Knight Brin"])
+soldier_2 = my_army.train_lancer("Nicklaus")''',
+                     test="soldier_2.introduce()",
+                     answer="Raubritter Nicklaus, European lancer")
         ],
     "3. My army": [
         prepare_test(middle_code='''my_army = EuropeanArmy()
-my_army.add_swordsman("Jaks")
-my_army.add_swordsman("Brin")
-my_army.add_lancer("Harold")
-my_army.add_archer("Robin")
-my_army.add_archer("May")''',
-                     test="my_army.show_lancers()",
-                     answer=["Raubritter Harold"])
+soldier_3 = my_army.train_archer("Beleron")''',
+                     test="soldier_3.introduce()",
+                     answer="Ranger Beleron, European archer")
         ],
     "4. My army": [
+        prepare_test(middle_code='''my_army = AsianArmy()
+soldier_4 = my_army.train_swordsman("Han")''',
+                     test="soldier_4.introduce()",
+                     answer="Samurai Han, Asian swordsman")
+        ],
+    "5. My army": [
+        prepare_test(middle_code='''my_army = AsianArmy()
+soldier_5 = my_army.train_lancer("Ayanami")''',
+                     test="soldier_5.introduce()",
+                     answer="Ronin Ayanami, Asian lancer")
+        ],
+    "6. My army": [
+        prepare_test(middle_code='''my_army = AsianArmy()
+soldier_6 = my_army.train_archer("Kitsune")''',
+                     test="soldier_6.introduce()",
+                     answer="Shinobi Kitsune, Asian archer")
+        ],
+    "7. My army": [
         prepare_test(middle_code='''my_army = EuropeanArmy()
-my_army.add_swordsman("Jaks")
-my_army.add_swordsman("Brin")
-my_army.add_lancer("Harold")
-my_army.add_archer("Robin")
-my_army.add_archer("May")''',
-                     test="my_army.show_archers()",
-                     answer=["Ranger Robin", "Ranger May"])
-        ]
+soldier_7 = my_army.train_swordsman("Blake")''',
+                     test="soldier_7.introduce()",
+                     answer="Knight Blake, Europen swordsman")
+        ],
+    "8. My army": [
+        prepare_test(middle_code='''my_army = EuropeanArmy()
+soldier_8 = my_army.train_lancer("Howard")''',
+                     test="soldier_8.introduce()",
+                     answer="Raubritter Howard, European lancer")
+        ],
+    "9. My army": [
+        prepare_test(middle_code='''my_army = EuropeanArmy()
+soldier_9 = my_army.train_archer("Alice")''',
+                     test="soldier_9.introduce()",
+                     answer="Ranger Alice, European archer")
+        ],
+    "10. My army": [
+        prepare_test(middle_code='''my_army = AsianArmy()
+soldier_10 = my_army.train_swordsman("Miyagi")''',
+                     test="soldier_10.introduce()",
+                     answer="Samurai Miyagi, Asian swordsman")
+        ],
+    "11. My army": [
+        prepare_test(middle_code='''my_army = AsianArmy()
+soldier_11 = my_army.train_lancer("Misa")''',
+                     test="soldier_11.introduce()",
+                     answer="Ronin Misa, Asian lancer")
+        ],
+    "12. My army": [
+        prepare_test(middle_code='''my_army = AsianArmy()
+soldier_12 = my_army.train_archer("Heroki")''',
+                     test="soldier_12.introduce()",
+                     answer="Shinobi Heroki, Asian archer")
+        ],
 }
